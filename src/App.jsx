5e61f22f1494e399779a51d7dff59db92cfc2b16
Ricardo <ricardofidelis@live.com>
2025-12-11 17:37:34 -0300

    feat: create WeatherInformationsForecast.jsx and config

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,32 @@ import WeatherInformations from "./components/WeatherInformations/WeatherInforma
 
 function App() {
   const [weather, setWeather] = useState();
+  const [weatherForecast, setWeatherForecast] = useState();
   const inputRef = useRef();
   async function searchCity() {
     const city = inputRef.current.value;
     const Key = "4c552b29d293015b8d1978af9f3e9e42";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Key}&units=metric&lang=pt_br`;
+    const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${Key}&units=metric&lang=pt_br`;
 
     const apiInfo = await axios.get(url);
+    const apiInfoForecast = await axios.get(urlForecast);
+
+    setWeatherForecast(apiInfoForecast.data);
     setWeather(apiInfo.data);
   }
   return (
     <>
-      <h1>Previsão do Tempo</h1>
-      <input ref={inputRef} type="text" placeholder="Cidade" />
-      <button onClick={searchCity}>Buscar</button>
+      <div className="container">
+        <h1>Previsão do Tempo</h1>
+        <input ref={inputRef} type="text" placeholder="Cidade" />
+        <button onClick={searchCity}>Buscar</button>
 
-      {weather && <WeatherInformations weather={weather} />}
+        {weather && <WeatherInformations weather={weather} />}
+        {weatherForecast && (
+          <weatherInformationForecast weatherForecast={weatherForecast} />
+        )}
+      </div>
     </>
   );
 }
